f7f350040a4c5207d3810106dae14998b413fd59
naemhui <knaem.edu@gmail.com>
2024-08-17 20:54:08 +0900

    0817 우주선 착륙

--- [0816-18] IM대비/10760_spaceship2.py
+++ [0816-18] IM대비/10760_spaceship2.py
@@ -5,6 +5,9 @@
 -> 예비 후보지 개수 출력
 '''
 
+def is_valid(r, c, N, M):
+    return 0<=r<N and 0<=c<M
+
 ## 상1 상2 상3 하1 하2 하3 좌 우
 dr = [-1, -1, -1, 1, 1, 1, 0, 0]
 dc = [-1, 0, 1, -1, 0, 1, -1, 1]
@@ -12,6 +15,20 @@ dc = [-1, 0, 1, -1, 0, 1, -1, 1]
 T = int(input())
 
 for tc in range(1, T+1):
-    N, M = map(int, input().split(0))
+    N, M = map(int, input().split())
     arr = [list(map(int,input().split())) for _ in range(N)]
-    
+    total = 0  # 예비 후보지 개수
+
+    for r in range(N):
+        for c in range(M):
+            cnt = 0  # 낮은 구역 개수 카운트
+            for d in range(8):
+                nr = r + dr[d]
+                nc = c + dc[d]
+
+                if is_valid(nr,nc,N,M):
+                    if arr[r][c] > arr[nr][nc]:
+                        cnt += 1
+            if cnt >= 4:
+                total += 1
+    print(f'#{tc} {total}')
